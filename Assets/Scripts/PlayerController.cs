dcd13143009170f7eed819dacc8e06f56da1f01a
BuildTools <noah@danielgarcia.info>
2019-03-20 20:52:14 -0500

    Fixes #2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
@@ -14,16 +14,25 @@ public class PlayerController : MonoBehaviour {
     AudioSource audioSource;
 
     private Animator animator;
+
+    public LayerMask Objective;
+
+    public Collider2D PlayerCollider;
     // Start is called before the first frame update
     void Start() {
         roverRigidbody = GetComponent<Rigidbody2D>();
         roverRenderer = GetComponent<SpriteRenderer>();
         audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
+        PlayerCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update() {
+        if (Physics2D.IsTouchingLayers(PlayerCollider, Objective)) {
+            Debug.Log("GOOOOAL!");
+            SceneManager.LoadScene("Win");
+        }
         roverRigidbody.velocity = new Vector2(roverRigidbody.velocity.x, roverRigidbody.velocity.y);
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
             jumpSelect = Random.Range(0, jumpSounds.Length);
@@ -43,5 +52,5 @@ public class PlayerController : MonoBehaviour {
             Debug.Log("Oops!");
             transform.position = new Vector3(-3.185597f, 1.416982f, 0);
         }
-    } 
+    }
 }
